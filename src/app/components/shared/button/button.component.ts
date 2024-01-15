1fca4e09fdcc83421a6a1202ec01bfb17edb38af
nastasiakh <kashchavtseva.a@gmail.com>
2024-01-15 10:38:55 +0200

    Edited button component:
    - added fields iconName and iconColor;
    - fixed html for using this functionality.

--- src/app/components/shared/button/button.component.ts
+++ src/app/components/shared/button/button.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input} from '@angular/core';
 import {NgIf, NgSwitch} from "@angular/common";
 import {MatButtonModule} from "@angular/material/button";
+import {MatIconModule} from "@angular/material/icon";
 
 @Component({
   selector: 'app-button',
@@ -8,6 +9,7 @@ import {MatButtonModule} from "@angular/material/button";
   imports: [
     NgSwitch,
     MatButtonModule,
+    MatIconModule,
     NgIf
   ],
   templateUrl: './button.component.html',
@@ -16,8 +18,10 @@ import {MatButtonModule} from "@angular/material/button";
 export class ButtonComponent {
 
   @Input() color: 'primary' | 'accent' | 'disabled' = 'primary';
+  @Input() iconColor?: 'primary' | 'accent' | 'warn' = 'accent';
   @Input() text: string = '';
   @Input() type: 'button' | 'link' = 'button';
   @Input() link?: string;
   @Input() disabled?: boolean = false;
+  @Input() iconName?: string;
 }
